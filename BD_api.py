36d9ebdc0ac30eb45133a6e3aed1f26dbd6f62da
daytrov <daytrov@gmail.com>
2024-10-05 13:39:13 +0300

    Добавил функцию get_img и convert_img в файл podpis.py.    get_img нужна для скачивания изображения подписи человека с сайта    convert_img нужна чтобы убрать белый фон с изображения Добавил комментарии к функциям

--- BD_api.py
+++ BD_api.py
@@ -3,11 +3,17 @@ import json
 import random
 from datetime import datetime
 
+
+#Функция get_data получает всю инфу и человеке с сайта
+#Ее не нужно вызывать отдельно, вызывается через first_iteration
 def get_data():
     response = requests.get('https://api.randomdatatools.ru/')
     data = response.json()
     return data
 
+
+#Заранее подготовленный лист с инфой о человеке.
+#Пустой до первой итерации
 personal_dict = {
     'LastName': '',
     'FirstName': '',
@@ -43,6 +49,10 @@ personal_dict = {
     'bankCVC': '',
 }
 
+
+#Функция convert_personal_info заполняет пустой лист (или заполненый инфой о прошлом человеке) инфой с api
+#Отельно вызывать не нужно, вызывается функцией get_info если новый человек, или first_iteration если первая итерация
+#Если потом будет нужно менять какую-либо инфу о человеке, делать это тут
 def convert_personal_info(data):
     personal_dict["LastName"] = data["LastName"]
     personal_dict["FirstName"] = data["FirstName"]
@@ -77,6 +87,11 @@ def convert_personal_info(data):
     personal_dict["bankCVC"] = data["bankCVC"]
     return personal_dict
 
+
+#Функция get_info основная функция в файле, возвращает лист с инфой о человеке.
+#Аргумент iteration:
+#True - получаем данные с api о новом человеке
+#False - работает с данными о старом человеке, который уже загружен в personal_dict
 def get_info(iteration):
     if iteration:
         data = get_data()
@@ -85,6 +100,9 @@ def get_info(iteration):
     else:
         return personal_dict
 
+
+#Функция first_iteration нужна для первого прогона программы, ее следует вызвать один раз перед началом работы.
+#Возможно ее потом удалим за ненадобностью
 def first_iteration():
     data = get_data()
     convert_personal_info(data)
--- podpis.py
+++ podpis.py
@@ -5,19 +5,45 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import PIL
+from PIL import Image
+import os
 
-
+#Фукнция get_img получает изображение подписи с сайта
+#На вход получает:
+#first_name - имя человека
+#last_name - фамилия человека
+#middle_name - отчество человека
+#Передавать аргументы в функция только в таком порядке
 def get_img(first_name, last_name, middle_name):
     driver = webdriver.Chrome()
     driver.get("https://podpis-online.ru/")
-    time.sleep(5)
+    time.sleep(2)
     driver.find_element(By.NAME, "name").send_keys(first_name)
     driver.find_element(By.NAME, "surname").send_keys(last_name)
     driver.find_element(By.NAME, "middle").send_keys(middle_name)
     driver.find_element(By.XPATH, "//div[@id='GetSignature']").click()
-    time.sleep(5)
+    time.sleep(2)
     canvas = driver.find_element(By.ID, "Signature_1")
     image_data = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
-    with open("signature.png", "wb") as f:
+    with open("not_converted_podpis.png", "wb") as f:
         f.write(base64.b64decode(image_data))
     driver.quit()
+    convert_img()
+
+#Функция convert_img удаляет белый фон у изображения и сохраняет его с названием podpis.png
+#Эту функцию не нужно вызавать, она запускается через get_img
+#После сохранения подписи без фона, удаляет старое изображение с фоном
+def convert_img():
+    img = Image.open("not_converted_podpis.png")
+    img = img.convert("RGBA")
+    pixels = img.getdata()
+    new_pix = []
+    for i in pixels:
+        if i[0] > 140 and i[1] > 140 and i[2] > 140:
+            new_pix.append((255, 255, 255, 0))
+        else:
+            new_pix.append(i)
+    img.putdata(new_pix)
+    img.save("podpis.png", "PNG")
+    os.remove("D:/PyCharmProjects/Docs_Please/not_converted_podpis.png")
