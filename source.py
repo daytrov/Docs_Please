ae4c66cb5198b2b1e8536871029017ab540008cc
daytrov <daytrov@gmail.com>
2024-11-02 16:31:43 +0300

    Изменения в коде и оптимизация

--- source.py
+++ source.py
@@ -5,9 +5,11 @@ import fontstyle
 BD_api.first_iteration()
 
 abitur_info = BD_api.get_info(False)
-print(abitur_info["LastName"])
+lier_abitur_info = BD_api.liar()
+print(abitur_info["FirstName"])
 print(abitur_info["DateOfBirth"])
 print(abitur_info["YearsOld"])
 print(abitur_info["PasportDate"])
+print(lier_abitur_info["FirstName"])
 print(fontstyle.apply(f"{abitur_info["LastName"]} {abitur_info["FirstName"][0]}.{abitur_info["FatherName"][0]}", 'ITALIC'))
 #podpis.get_img(abitur_info["FirstName"], abitur_info["LastName"], abitur_info["FatherName"])
